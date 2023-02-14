d916f541af4a016fa233b93557c5cc4127cbf6a7
Artemides <edmpulasky@gmail.com>
2023-02-14 10:13:56 -0500

    wrap node

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,127 @@
-import { useCallback, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 /* eslint-disable */ //This wrapper comments it to use react-map-interaction without types
 // @ts-ignore
 import { MapInteractionCSS } from "react-map-interaction";
-import { Step, useInteractiveTutorial } from './useInteractiveTutorial';
+import { Step, useInteractiveTutorial } from "./useInteractiveTutorial";
 /* eslint-enable */
 
 type Node = {
-  id: string,
-  title: string,
-  top: number,
-  left: number
-}
+  id: string;
+  title: string;
+  top: number;
+  left: number;
+};
 
 const steps: Step[] = [
   {
     id: "n1",
     title: "step 1",
-    description: "step-1"
-  }, {
+    description: "step-1",
+  },
+  {
     id: "n2",
     title: "step 2",
-    description: "step-2"
-  }, {
+    description: "step-2",
+  },
+  {
     id: "n3",
     title: "step 3",
-    description: "step-3"
-  }
-]
+    description: "step-3",
+  },
+];
 
 const InitialNodes: Node[] = [
-  { id: 'n1', title: "node title 1", top: 0, left: 0 },
-  { id: 'n2', title: "node title 2", top: 0, left: 0 },
-  { id: 'n3', title: "node title 3", top: 0, left: 0 },
-]
-
+  { id: "n1", title: "node title 1", top: 0, left: 0 },
+  { id: "n2", title: "node title 2", top: 0, left: 0 },
+  { id: "n3", title: "node title 3", top: 0, left: 0 },
+];
+const DEFAULT_NUMBER_OF_TRIES = 5;
 function App() {
-  const [count, setCount] = useState(0)
-  const [nodes, setNodes] = useState(InitialNodes)
-  const { currentStepIdx: currentStep, nextStep, previousStep, StepComponent, setCurrentStepIdx: setCurrentStep } = useInteractiveTutorial({ steps })
+  const [count, setCount] = useState(0);
+  const [nodes, setNodes] = useState(InitialNodes);
+
+  const [obj, setObj] = useState({ width: 0, height: 0, top: 0, left: 0 });
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const {
+    currentStepIdx: currentStep,
+    nextStep,
+    previousStep,
+    StepComponent,
+    setCurrentStepIdx: setCurrentStep,
+  } = useInteractiveTutorial({ steps });
 
   const setRandomPosition = useCallback(() => {
-    const randomNodes = nodes.map(cur => ({ ...cur, left: getRandom(500), top: getRandom(400) }))
-    setNodes(randomNodes)
-  }, [nodes])
+    const randomNodes = nodes.map((cur) => ({
+      ...cur,
+      left: getRandom(500),
+      top: getRandom(400),
+    }));
+    setNodes(randomNodes);
+  }, [nodes]);
+
+  const observeTries2 = useRef(0);
+
+  const [id, setId] = useState("");
 
   useEffect(() => {
-    setRandomPosition()
-  }, [])
+    if (!nodes[currentStep]) return;
+
+    const intervalId = setInterval(() => {
+      if (observeTries2.current >= DEFAULT_NUMBER_OF_TRIES) {
+        observeTries2.current = 0;
+        clearInterval(intervalId);
+        return;
+      }
+
+      observeTries2.current += 1;
+      const element = document.getElementById(nodes[currentStep].id);
+      if (!element) return;
+
+      setObj({
+        width: element.clientWidth,
+        height: element.clientHeight,
+        top: element.offsetTop,
+        left: element.offsetLeft,
+      });
+      observeTries2.current = 0;
+      clearInterval(intervalId);
+    }, 200);
+
+    return () => {
+      clearInterval(intervalId);
+
+      return;
+    };
+  }, [currentStep]);
+
+  useEffect(() => {
+    setRandomPosition();
+  }, []);
+
+  useLayoutEffect(() => {
+    if (!ref.current) return;
+
+    setObj({
+      width: ref.current.clientWidth,
+      height: ref.current.clientHeight,
+      top: ref.current.offsetTop,
+      left: ref.current.offsetLeft,
+    });
+    console.log({
+      width: ref.current.clientWidth,
+      height: ref.current.clientHeight,
+      top: ref.current,
+    });
+  }, [nodes]);
 
   return (
     <div className="App">
@@ -66,27 +139,98 @@ function App() {
         </button>
       </div>
 
-      <div style={{ border: "solid 2px pink", width: "800px", height: "500px" }}>
-        <button onClick={previousStep}>{"<<"}</button>
-        <button onClick={() => setCurrentStep(0)} >start</button>
-        <button onClick={nextStep}>{">>"}</button>
-        <button onClick={setRandomPosition}>{"move"}</button>
-        <MapInteractionCSS >
-          {nodes.map(cur => <div key={cur.id} id={cur.id} style={{ position: "absolute", top: cur.top, left: cur.left, border: "solid 2px royalBlue" }}>
-            {cur.title}
-          </div>)}
+      <button onClick={previousStep}>{"<<"}</button>
+      <button onClick={() => setCurrentStep(0)}>start</button>
+      <button onClick={nextStep}>{">>"}</button>
+      <button onClick={setRandomPosition}>{"move"}</button>
+      <div
+        id="container"
+        style={{ border: "solid 2px pink", width: "800px", height: "500px" }}
+      >
+        <MapInteractionCSS>
+          <div
+            style={{
+              position: "absolute",
+              top: "0px",
+              left: "0px",
+              width: "4px",
+              height: "4px",
+              borderRadius: "50%",
+              backgroundColor: "red",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: "0px",
+              left: "100px",
+              width: "4px",
+              height: "4px",
+              borderRadius: "50%",
+              backgroundColor: "green",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: "0px",
+              left: "200px",
+              width: "4px",
+              height: "4px",
+              borderRadius: "50%",
+              backgroundColor: "blue",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: "0px",
+              left: "300px",
+              width: "4px",
+              height: "4px",
+              borderRadius: "50%",
+              backgroundColor: "purple",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: `${obj.top}px`,
+              left: `${obj.left}px`,
+              width: `${obj.width}px`,
+              height: `${obj.height}px`,
+              backgroundColor: "transparent",
+              outline: "5000px solid #555555bd",
+              outlineOffset: "10px",
+              boxSizing: "border-box",
+              zIndex:999
+            }}
+          ></div>
+          {nodes.map((cur) => (
+            <div
+              ref={cur.id === "n1" ? ref : null}
+              key={cur.id}
+              id={cur.id}
+              style={{
+                position: "absolute",
+                top: cur.top,
+                left: cur.left,
+                border: "solid 2px royalBlue",
+                width: "100px",
+                boxSizing: "border-box",
+              }}
+            >
+              {cur.title}
+            </div>
+          ))}
         </MapInteractionCSS>
       </div>
 
-
-
       {StepComponent}
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
-
-const getRandom = (max = 10) => Math.floor(Math.random() * max) + 1.
+const getRandom = (max = 10) => Math.floor(Math.random() * max) + 1;
--- src/useInteractiveTutorial.tsx
+++ src/useInteractiveTutorial.tsx
@@ -1,104 +1,166 @@
-import React, { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
+import React, { ReactNode, useEffect, useMemo, useRef, useState } from "react";
 
 export type Step = {
-    id: string,
-    title: string,
-    description: string
-}
+  id: string;
+  title: string;
+  description: string;
+};
 
-type UseInteractiveTutorialProps = { steps: Step[] }
+type UseInteractiveTutorialProps = { steps: Step[] };
 
-const DEFAULT_NUMBER_OF_TRIES = 5
+const DEFAULT_NUMBER_OF_TRIES = 5;
 
-export const useInteractiveTutorial = ({ steps }: UseInteractiveTutorialProps) => {
-    const [currentStepIdx, setCurrentStepIdx] = useState(0)
-    const observer = useRef<ResizeObserver | null>(null);
-    const observeTries = useRef(0);
+export const useInteractiveTutorial = ({
+  steps,
+}: UseInteractiveTutorialProps) => {
+  const [currentStepIdx, setCurrentStepIdx] = useState(0);
+  const observer = useRef<ResizeObserver | null>(null);
+  const intersectionObserver = useRef<IntersectionObserver | null>(null);
+  const observeTries = useRef(0);
+  const observeTries2 = useRef(0);
 
-    useEffect(() => {
-        // height, top we need to changes
-        observer.current = new ResizeObserver(entries => {
-            try {
-                const rr = entries[0].contentRect
-                console.log({ rr })
-                // const topPosition = (entries[0].target as any)?.style?.top;
-                // const isSimilar = blockSize === previousHeightRef.current;
-                // previousHeightRef.current = blockSize;
-                // previousTopRef.current = topPosition;
-                // if (isSimilar) return;
+  const [id, setId] = useState("");
 
-                // changeNodeHight(identifier, blockSize);
-            } catch (err) {
-                console.warn("invalid entry", err);
-            }
-        });
-        console.log('observer configured')
-        return () => {
-            if (!observer.current) return;
-            return observer.current.disconnect();
-        };
-    }, []);
+  useEffect(() => {
+    if (!id) return;
 
-    useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (observeTries2.current >= DEFAULT_NUMBER_OF_TRIES) {
+        observeTries2.current = 0;
+        clearInterval(intervalId);
+        return;
+      }
 
-        if (!observer?.current) return
-        if (currentStepIdx < 0 || currentStepIdx >= steps.length) {
-            return observer.current.disconnect();
-        }
+      observeTries2.current += 1;
+      const element = document.getElementById(id);
+      if (!element) return;
 
+      observeTries2.current = 0;
+      clearInterval(intervalId);
+    }, 200);
 
+    return () => {
+      clearInterval(intervalId);
 
-        const currentStep = steps[currentStepIdx]
-        console.log('try to ovbser', { id: currentStep.id })
+      return;
+    };
+  }, [id]);
 
-        const intervalId = setInterval(() => {
-            if (observeTries.current >= DEFAULT_NUMBER_OF_TRIES) {
-                observeTries.current = 0
-                clearInterval(intervalId)
-                return
-            }
+  const container = document.getElementById("container");
+  const options = {
+    root: container,
+    rootMargin: "0px",
+    threshold: 1.0,
+  };
+  console.log({ options });
+  useEffect(() => {
+    // height, top we need to changes
+    intersectionObserver.current = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      console.log({ entry });
+    }, options);
+    observer.current = new ResizeObserver((entries) => {
+      try {
+        const rr = entries[0].contentRect;
+        console.log({ rr });
+        // const topPosition = (entries[0].target as any)?.style?.top;
+        // const isSimilar = blockSize === previousHeightRef.current;
+        // previousHeightRef.current = blockSize;
+        // previousTopRef.current = topPosition;
+        // if (isSimilar) return;
 
-            observeTries.current += 1
-            const element = document.getElementById(currentStep.id)
-            if (!element) return
-            if (!observer?.current) return
+        // changeNodeHight(identifier, blockSize);
+      } catch (err) {
+        console.warn("invalid entry", err);
+      }
+    });
+    console.log("observer configured");
+    return () => {
+      if (!observer.current) return;
 
-            observer.current.observe(element);
-            console.log('observe element', { element })
-            observeTries.current = 0
-            clearInterval(intervalId)
-        }, 200)
+      observer.current.disconnect();
+      if (!intersectionObserver.current) return;
 
-        return () => {
-            clearInterval(intervalId)
-            if (!observer.current) return;
-            return observer.current.disconnect();
-        };
-    }, [currentStepIdx]);
+      intersectionObserver.current.disconnect();
 
-    const nextStep = () => {
-        if (currentStepIdx < 0) return
-        if (currentStepIdx === steps.length - 1) return setCurrentStepIdx(-1)
-        setCurrentStepIdx(prev => prev + 1)
+      return;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!observer?.current || !intersectionObserver.current) return;
+    if (currentStepIdx < 0 || currentStepIdx >= steps.length) {
+      observer.current.disconnect();
+      intersectionObserver.current.disconnect();
+      return;
     }
-    const previousStep = () => {
-        if (currentStepIdx <= 0) return
-        setCurrentStepIdx(prev => prev - 1)
-    }
-    // const completeTutorial = () => setCurrentStep(-1)
 
-    const StepComponent: ReactNode = useMemo(() => {
-        if (currentStepIdx < 0) return null
-        if (currentStepIdx >= steps.length) return null
-        const currentStep = steps[currentStepIdx].title
+    const currentStep = steps[currentStepIdx];
+    console.log("try to ovbser", { id: currentStep.id });
 
-        // const element = document.getElementById()
+    const intervalId = setInterval(() => {
+      if (observeTries.current >= DEFAULT_NUMBER_OF_TRIES) {
+        observeTries.current = 0;
+        clearInterval(intervalId);
+        return;
+      }
 
-        return <div style={{ border: "solid 2px red" }}>
-            <h5>{steps[currentStepIdx].title}</h5>
-            <p>{steps[currentStepIdx].description}</p>
-        </div>
-    }, [currentStepIdx])
+      observeTries.current += 1;
+      const element = document.getElementById(currentStep.id);
+      if (!element) return;
+      if (!observer.current || !intersectionObserver.current) return;
 
-    return { currentStepIdx, nextStep, previousStep, setCurrentStepIdx, StepComponent }
-}
+      observer.current.observe(element);
+      intersectionObserver.current.observe(element);
+      console.log("observe element", { element });
+      observeTries.current = 0;
+      clearInterval(intervalId);
+    }, 200);
+
+    return () => {
+      clearInterval(intervalId);
+      if (!observer.current) return;
+
+      observer.current.disconnect();
+      if (!intersectionObserver.current) return;
+
+      intersectionObserver.current.disconnect();
+
+      return;
+    };
+  }, [currentStepIdx]);
+
+  const nextStep = () => {
+    if (currentStepIdx < 0) return;
+    if (currentStepIdx === steps.length - 1) return setCurrentStepIdx(-1);
+    setCurrentStepIdx((prev) => prev + 1);
+  };
+  const previousStep = () => {
+    if (currentStepIdx <= 0) return;
+    setCurrentStepIdx((prev) => prev - 1);
+  };
+  // const completeTutorial = () => setCurrentStep(-1)
+
+  const StepComponent: ReactNode = useMemo(() => {
+    if (currentStepIdx < 0) return null;
+    if (currentStepIdx >= steps.length) return null;
+    const currentStep = steps[currentStepIdx].title;
+
+    // const element = document.getElementById()
+
+    return (
+      <div style={{ border: "solid 2px red" }}>
+        <h5>{steps[currentStepIdx].title}</h5>
+        <p>{steps[currentStepIdx].description}</p>
+      </div>
+    );
+  }, [currentStepIdx]);
+
+  return {
+    currentStepIdx,
+    nextStep,
+    previousStep,
+    setCurrentStepIdx,
+    StepComponent,
+  };
+};
